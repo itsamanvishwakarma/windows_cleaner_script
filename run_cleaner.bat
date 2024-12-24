3682335e24291eb4ef85e703f56dd3c8a48e7234
Aman Vishwakarma <vishwakarmaaman8000@gmail.com>
2024-12-24 14:16:06 +0530

    removes miniconda env inti - now using pyenv-win

--- run_cleaner.bat
+++ run_cleaner.bat
@@ -1,4 +1,3 @@
 @echo off
-call C:\Users\itsam\miniconda3\Scripts\activate.bat windows_cleaner-script
-python C:\Users\itsam\Developer\windows_cleaner_script\windows_cleaner-script.py
+python C:\Users\itsam\Code\windows_cleaner_script\windows_cleaner-script.py
 pause
